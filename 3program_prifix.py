2b78aba0a110f6228b902e6afb613a1c33518a44
Abdul Muhmin S <abdul.muhmin@vaisesika.local>
2024-10-10 02:42:16 -0700

    day 2 report

--- 3program_prifix.py
+++ 3program_prifix.py
@@ -1,13 +1,15 @@
 def common_prefix(lst):
     
 
-    # Start by assuming the first string is the common prefix
-    prefix = lst[0]
+    
+    prefix = lst[0]     # assuming the first string is the common prefix
 
-    for string in lst[1:]:
+    for string in lst[1:]:  #looping from second sring from the lst
+
         # Reduce the prefix as long as the current string doesn't start with it
         while not string.startswith(prefix):    #If the current string does not start with the prefix, the loop will execute to reduce the prefix.
             prefix = prefix[:-1]  # Shorten the prefix
+            
             if not prefix:  # If prefix becomes empty
                 return False  # Return False if there's no common prefix
 
